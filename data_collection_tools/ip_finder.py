bb13f94bb99774d9751996dcf809f2eeafe7a17d
Pilladian <p.zimmermann@usrmail.de>
2020-11-30 11:43:24 +0100

    Update ip_finder.py

--- data_collection_tools/ip_finder.py
+++ data_collection_tools/ip_finder.py
@@ -4,19 +4,22 @@ import subprocess
 import shlex
 
 
-class IPFinder:
+# return a list of pairs (subdomain, [all_ips_of_that_domain])
+def get_all_ips(subdomain_list):
+    ip_addresses = []
 
-    def __init__(self, _main_ip, _main_domain):
-        self.ip = _main_ip
-        self.domain = _main_domain
+    for subdomain in subdomain_list:
+        ips = _get_ip(subdomain)
+        ip_addresses.append((subdomain, ips))
 
-    def collect(self):
-        ips = [self.ip]
+    return ip_addresses
 
-        cmd = f'host {self.domain}'
 
-        proc = subprocess.Popen(shlex.split(cmd), stdout=subprocess.PIPE)
-        out, err = proc.communicate()
-        out = out.decode('utf-8').split("\n")
-        print(out)
-        return ips
+# returns list of all ips that can be found for a given domain
+def _get_ip(domain):
+    ips = []
+    cmd = f'host {domain}'
+    proc = subprocess.Popen(shlex.split(cmd), stdout=subprocess.PIPE)
+    out, err = proc.communicate()
+    out = out.decode('utf-8').split("\n")
+    return ips
