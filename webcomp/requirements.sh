7f4725bd4b6561df72984991ebffbd3ec9beea97
Pilladian <p.zimmermann@usrmail.de>
2021-02-21 15:54:35 +0100

    added requirements .sh and .txt

--- webcomp/requirements.sh
+++ webcomp/requirements.sh
@@ -1,3 +1,8 @@
-sudo apt install chromium-chromedriver
-
-pip3 install selenium
+apt install chromium-chromedriver -y
+apt install ffmpeg -y
+apt install libsm6 -y
+apt install libxext6 -y
+apt install chomium -y
+apt install chromium-browser -y
+apt chromium-browser-l10n -y
+apt install chromium-codecs-ffmpeg -y
