4e6db5d8a5483afa0d797003d4cc960c1668bac5
Pilladian <p.zimmermann@usrmail.de>
2021-03-01 08:28:13 +0100

    Update on requirements

--- requirements/requirements.sh
+++ requirements/requirements.sh
@@ -1,8 +1,7 @@
-apt install chromium-chromedriver -y
 apt install ffmpeg -y
 apt install libsm6 -y
 apt install libxext6 -y
-apt install chomium -y
-apt install chromium-browser -y
-apt chromium-browser-l10n -y
+apt install chromium -y
+apt install chromium-browser=1:85.0.4183.83-0ubuntu0.20.04.2 -y
+apt install chromium-chromedriver -y
 apt install chromium-codecs-ffmpeg -y
